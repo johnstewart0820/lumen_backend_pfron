5f50fcad8b855adc59002c28a98bb2a44897067a
johnstewart0820 <john.s@outsource.codes>
2021-04-14 00:40:44 +0300

    Modify: fix the placeseeder

--- database/seeders/PlaceSeeder.php
+++ database/seeders/PlaceSeeder.php
@@ -25,7 +25,7 @@ class PlaceSeeder extends Seeder
             if ($data[5] === 'województwo') {
                 \App\Models\Voivodeship::create(['id' => $data[0],'name' => $data[4]]);
                 $id_voivodeship = $data[0];
-            } else if ($data[5] === 'powiat') {
+            } else if ($data[5] === 'powiat' || $data[5] === 'miasto na prawach powiatu') {
                 \App\Models\County::create(['name' => $data[4], 'voivodeship_id' => $id_voivodeship]);
                 $id_county++;
             } else if ($data[5] !== 'NAZWA_DOD') {
